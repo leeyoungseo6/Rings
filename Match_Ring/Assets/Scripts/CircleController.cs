22165f7f4c404c611dca310fdc711a2e88d19c0e
luca3 <dudsj9782@gmail.com>
2023-09-16 02:18:28 +0900

    a

--- Match_Ring/Assets/Scripts/CircleController.cs
+++ Match_Ring/Assets/Scripts/CircleController.cs
@@ -13,8 +13,8 @@ public class CircleController : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
-            float scale = 1 + _mainCam.ScreenToWorldPoint(Input.mousePosition).magnitude / 3;
-            transform.localScale = new Vector3(scale, scale, scale);
+            float scale = Mathf.Clamp(-3 + ((Vector2)_mainCam.ScreenToWorldPoint(Input.mousePosition)).magnitude * 2, 1, 5);
+            transform.localScale = new Vector3(scale, scale, 1);
         }
     }
 }
